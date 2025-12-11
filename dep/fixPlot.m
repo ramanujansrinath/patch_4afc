efa51a51fd8ac4e75aa204460b5be7d731f54a33
Ramanujan Srinath <8876869+ramanujansrinath@users.noreply.github.com>
2025-12-11 15:14:50 -0600

    some dependencies

---
+++ dep/fixPlot.m
@@ -0,0 +1,48 @@
+function fixPlot(h,xLims,yLims,xLabel,yLabel,xTick,yTick,titleStr,legendStr)
+    axes(h); h = gca; % just in case
+    h.LineWidth = 2; h.Color = 'w';
+    h.XColor = 'k'; h.YColor = 'k';
+    h.Box = 'off';
+    h.TickDir = 'out'; h.LineWidth = 2;
+    h.GridAlpha = 0.05;
+    h.GridLineStyle = '--';
+    
+    if ~isempty(xLims); h.XLim = round(xLims,2); end
+    if ~isempty(yLims); h.YLim = round(yLims,2); end
+         
+    if ~exist('xTick','var'); xTick = []; end
+    if ~exist('yTick','var'); yTick = []; end
+    
+    if ~isempty(xTick)
+        h.XTick = round(xTick,2);
+    else
+        h.XTick = linspace(h.XLim(1),h.XLim(2),5); 
+    end
+    if ~isempty(yTick) 
+        h.YTick = round(yTick,2); 
+    else
+        h.YTick = linspace(h.YLim(1),h.YLim(2),5); 
+    end
+     
+    h.FontSize = 12; h.FontName = 'Lato';
+     
+    h.XLabel.String = xLabel;
+    h.XLabel.FontSize = 14; h.XLabel.FontName = 'Lato';
+    h.YLabel.String = yLabel;
+    h.YLabel.FontSize = 14; h.YLabel.FontName = 'Lato';
+     
+    if exist('legendStr','var')
+        hl = legend(h,legendStr);
+        hl.FontSize = 14; hl.TextColor = 'k'; hl.Color = 'w'; hl.Box = 'off';
+        hl.Location = 'best';
+    end
+     
+    if exist('titleStr','var')
+        ht = title(h,titleStr);
+        ht.Interpreter = 'none';
+        ht.Color = 'k'; ht.FontSize = 16; ht.FontName = 'Lato';
+    end
+    
+    axis(h,'square');
+    grid(h,'on');
+end
